9700930bdfaac17923218e154afe24f5b9acb82b
Sunny (SunJae) Lee 이선재 <snjlee58@gmail.com>
2024-07-16 14:36:26 +0900

    Added pollJobStatus function to track job status periodically

--- src/components/SearchSetting.vue
+++ src/components/SearchSetting.vue
@@ -12,13 +12,11 @@
       <v-text-field label="Job ID" variant="underlined"></v-text-field>
       <v-text-field label="Max RAM" variant="underlined"></v-text-field>
 
-      <v-btn :loading="waiting" @click="clickButton">
+      <v-btn :loading="waiting" @click="sendRequest">
         Click
       </v-btn>
     </v-card>
 
-    
-  
   </v-container>
 
   <v-container>
@@ -45,33 +43,65 @@ export default {
       q1: '/Users/sunnylee/Documents/Steinegger Lab/metabuli_example/SRR14484345_1.fq',
       q2: '/Users/sunnylee/Documents/Steinegger Lab/metabuli_example/SRR14484345_2.fq',
       database: "/Users/sunnylee/Documents/Steinegger Lab/metabuli_example/refseq_virus",
-      jobid: "TEST78",
+      jobid: "TEST99",
       outdir: "/Users/sunnylee/Documents/Steinegger Lab/metabuli_example",
       maxram: 128
     },
+    status: '',
     waiting: false
   }),
-  
+
   methods: {
-    async clickButton () {
+    // Function to populate formData
+    populateFormData() {
       this.formData.append('q1', this.jobDetails.q1);
       this.formData.append('q2', this.jobDetails.q2);
       this.formData.append('database[]', this.jobDetails.database);
       this.formData.append('jobid', this.jobDetails.jobid);
       this.formData.append('outdir', this.jobDetails.outdir);
       this.formData.append('maxram', this.jobDetails.maxram);
-
+    },
+    async sendRequest () {
+      this.populateFormData();
 
       axios.post('/api/ticket', this.formData) //FIXME: JSONPlaceholder API -> replace with Metabuli endpoint
-        .then(response => {
-          console.log('Response:', response.data);
-          // this.results.push(response.data); // Add the response to results
-          // this.saveResults(); // Save the results content
+        .then(async response => {
+          console.log('success', response.data);
+
+          try {
+            await this.pollJobStatus(response.data.id); // Wait until job completes
+            console.log("COMPLETE") //DELETE
+            // const reportFilePath = `${this.jobDetails.jobid}_report.tsv`;
+            // this.readTSVFile(`${this.jobDetails.outdir}/${reportFilePath}`);
+          } catch (error) {
+            console.error('Error waiting for job completion:', error);
+          }
         })
         .catch(error => {
-          console.error('Error posting data:', error);
+          console.error('error', error);
         });
-    }
+    },
+    // Function to track job status
+    async pollJobStatus(ticketid, interval = 500, timeout = 300000) { // Check job status every 0.5 seconds, timeout after 30 seconds
+      const start = Date.now();
+      while (Date.now() - start < timeout) {
+        try {
+          const response = await axios.get(`/api/ticket/${ticketid}`); // Get job status
+          console.log(response.data.status) //DELETE
+          if (response.data.status === 'RUNNING') {
+            this.status = "RUNNING"
+          }
+          else if (response.data.status === 'COMPLETE') {
+            this.status = "COMPLETE"
+            return true;
+          }
+        } catch (error) {
+          console.error('Error polling job status:', error);
+        }
+        await new Promise(resolve => setTimeout(resolve, interval));
+      }
+      throw new Error('Polling timed out');
+    },
 
   } 
 }
