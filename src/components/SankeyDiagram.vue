0e673820d10d978bf6b8bf6011142a61e9bdef68
Sunny (SunJae) Lee 이선재 <snjlee58@gmail.com>
2024-07-24 10:52:29 +0900

    Edited style for node detail sheet

--- src/components/SankeyDiagram.vue
+++ src/components/SankeyDiagram.vue
@@ -3,7 +3,12 @@
     <div ref="sankeyContainer" class="sankey-diagram"></div>
 
     <!-- NODE OR LINK DETAILS PANEL -->
-    <v-sheet class="details-sheet" elevation="2">
+    <v-sheet class="d-flex align-center justify-center flex-wrap text-center mx-auto my-4 px-4"
+     elevation="4"
+      max-width="800"
+      width="100%"
+      rounded
+    >
       <v-card-text v-if="hoverDetails">
         <div v-if="hoverDetails.type === 'node'">
 
@@ -410,10 +415,6 @@
 .sankey-diagram {
   flex: 1;
 }  
-.details-sheet {
-  /* width: 300px; */
-  /* height: 500px; */
-}
 .tab {
   margin-left: 15px;
 }
