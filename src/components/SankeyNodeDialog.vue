0eca547e2d0f7a0763b1b001caeaad45975561e6
Sunny (SunJae) Lee 이선재 <snjlee58@gmail.com>
2024-07-25 14:09:31 +0900

    Edited indentation

--- src/components/SankeyNodeDialog.vue
+++ src/components/SankeyNodeDialog.vue
@@ -1,45 +1,47 @@
 <template>
     <v-dialog class="d-flex align-center justify-center flex-wrap text-center mx-auto my-4 px-4" v-model="localDialog" max-width="window.innerWidth">
-      <v-card>
-        <v-card-title>
-          <span>{{ hoverDetails.data.name }}</span>
-        </v-card-title>
-        <v-card-text>
-          <!-- <p v-if="hoverDetails.type === 'link'"><strong>Link:</strong> {{ hoverDetails.data.source.name }} → {{ hoverDetails.data.target.name }}</p> -->
-          <p>Rank <strong>{{ hoverDetails.data.rank }}</strong></p>
+        <!-- NODE DETAILS -->
+        <v-card>
+            <v-card-title>{{ hoverDetails.data.name }}</v-card-title>
+            <v-card-text>
+                <!-- <p v-if="hoverDetails.type === 'link'"><strong>Link:</strong> {{ hoverDetails.data.source.name }} → {{ hoverDetails.data.target.name }}</p> -->
+                <p>Rank <strong>{{ hoverDetails.data.rank }}</strong></p>
 
-          <p>TaxID {{ hoverDetails.data.id }}</p>
-          <div class="taxid-breadcrumbs">
-            <p>
-              <a :href="`https://www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi?mode=Info&id=${hoverDetails.data.id}`" target="_blank">
-                NCBI Taxonomy
-              </a> 
-              /
-              <a :href="`https://www.ncbi.nlm.nih.gov/assembly/?term=txid${hoverDetails.data.id}[Organism:exp]`" target="_blank">
-                Assemblies
-              </a>
-              /
-              <a :href="`https://pubmed.ncbi.nlm.nih.gov/?term=${hoverDetails.data.name}`" target="_blank">
-                PubMed
-              </a>
-            </p>
-          </div>
-          
-          <br>
+                <p>TaxID {{ hoverDetails.data.id }}</p>
+                <div class="taxid-breadcrumbs">
+                    <p>
+                    <a :href="`https://www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi?mode=Info&id=${hoverDetails.data.id}`" target="_blank">
+                        NCBI Taxonomy
+                    </a> 
+                    /
+                    <a :href="`https://www.ncbi.nlm.nih.gov/assembly/?term=txid${hoverDetails.data.id}[Organism:exp]`" target="_blank">
+                        Assemblies
+                    </a>
+                    /
+                    <a :href="`https://pubmed.ncbi.nlm.nih.gov/?term=${hoverDetails.data.name}`" target="_blank">
+                        PubMed
+                    </a>
+                    </p>
+                </div>
+                
+                <br>
 
-          <p><strong>Proportion:</strong> {{ hoverDetails.data.proportion }}%</p>
-          <p><strong>Clade Reads:</strong> {{ hoverDetails.data.clade_reads }}</p>
-          <p><strong>Taxon Reads:</strong> {{ hoverDetails.data.taxon_reads }}</p>
-          
-          <br>
-          
-          <p><strong>Lineage:</strong> {{ hoverDetails.data.lineage.map(n => `${n.name} (${n.rank})`).join(' > ') }}</p>
+                <p><strong>Proportion:</strong> {{ hoverDetails.data.proportion }}%</p>
+                <p><strong>Clade Reads:</strong> {{ hoverDetails.data.clade_reads }}</p>
+                <p><strong>Taxon Reads:</strong> {{ hoverDetails.data.taxon_reads }}</p>
+                
+                <br>
+                
+                <p><strong>Lineage:</strong> {{ hoverDetails.data.lineage.map(n => `${n.name} (${n.rank})`).join(' > ') }}</p>
+                
+            </v-card-text>
             
-        </v-card-text>
-        <v-card-actions>
-          <v-btn color="primary" @click="closeDialog">Close</v-btn>
-        </v-card-actions>
-      </v-card>
+            <v-card-actions>
+                <v-btn color="primary" @click="closeDialog">Close</v-btn>
+            </v-card-actions>
+        </v-card>
+
+        <!-- NODE SUBTREE SANKEY DIAGRAM -->
     </v-dialog>
   </template>
   
