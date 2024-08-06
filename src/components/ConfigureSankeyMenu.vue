b7cbe6861862e53c16237b56f14b4831d10f8ce1
Sunny (SunJae) Lee 이선재 <snjlee58@gmail.com>
2024-08-07 01:45:41 +0900

    Added expansion panels to configure sankey menu

--- src/components/ConfigureSankeyMenu.vue
+++ src/components/ConfigureSankeyMenu.vue
@@ -13,128 +13,141 @@
         <slot name="activator" v-bind="{ props }"></slot>
       </template>
 
-      <v-card min-width="300" class=" my-1">
+      <v-card width="340" class="my-1 configure-menu">
         <v-form ref="form">
-          <v-list>
-            <!-- SWITCH (Show all) -->
-            <v-list-item>
-              <v-container class="pt-1">
-                <div class="d-flex align-center gc-2">
-                  <v-switch
-                    v-model="showAll"
-                    color="indigo"
-                    hide-details
-                    @update:modelValue="validateRange"
-                  ></v-switch>
-                  <div class="text-caption">Show all</div>
-                </div>
-              </v-container>
-            </v-list-item>
+          <!-- SWITCH (Show all) -->
+          <v-list-item>
+            <v-container class="pt-1">
+              <div class="d-flex align-center gc-2">
+                <v-switch
+                  v-model="showAll"
+                  color="indigo"
+                  hide-details
+                  @update:modelValue="validateRange"
+                ></v-switch>
+                <div class="text-caption">Show all</div>
+              </div>
+            </v-container>
+          </v-list-item>
 
-            <v-divider></v-divider>
+          <v-divider></v-divider>
 
-            <!-- SWITCH (Show/Hide Unclassified Nodes) -->
-            <v-list-item>
-              <v-container class="pt-1">
-                <div class="d-flex align-center gc-2">
-                  <v-switch
-                    v-model="tempShowUnclassified"
-                    color="indigo"
-                    hide-details
-                    :disabled="showAll"
-                  ></v-switch>
-                  <div class="text-caption">Show unclassified taxa</div>
-                </div>
-              </v-container>
-            </v-list-item>
+          <v-expansion-panels
+            v-model="activePanel"
+            variant="accordion"
+            elevation="0"
+          >
+            <v-expansion-panel title="Node Settings">
+              <!-- GRAPH SETTINGS -->
+              <v-expansion-panel-text>
+                <!-- SWITCH (Show/Hide Unclassified Nodes) -->
+                <v-list-item>
+                  <v-container class="pt-1">
+                    <div class="d-flex align-center gc-2">
+                      <v-switch
+                        v-model="tempShowUnclassified"
+                        color="indigo"
+                        hide-details
+                        :disabled="showAll"
+                      ></v-switch>
+                      <div class="text-caption">Show unclassified taxa</div>
+                    </div>
+                  </v-container>
+                </v-list-item>
 
-            <!-- DROPDOWN AND INPUT (Minimum Clade Reads) -->
-            <v-list-item>
-              <div class="text-caption">Minimum number of reads</div>
-              <v-container class="d-flex align-center">
-                <v-select
-                  v-model="tempCladeReadsMode"
-                  :items="['%', '#']"
-                  variant="underlined"
-                  density="compact"
-                  dense
-                  class="select-width"
-                  @change="setDefaultValue"
-                  :disabled="showAll"
-                ></v-select>
+                <!-- DROPDOWN AND INPUT (Minimum Clade Reads) -->
+                <v-list-item>
+                  <div class="text-caption">Minimum number of reads</div>
+                  <v-container class="d-flex align-center">
+                    <v-select
+                      v-model="tempCladeReadsMode"
+                      :items="['%', '#']"
+                      variant="underlined"
+                      density="compact"
+                      dense
+                      class="select-width"
+                      @change="setDefaultValue"
+                      :disabled="showAll"
+                    ></v-select>
 
-                <v-text-field
-                  v-model="tempCladeReadsValue"
-                  type="number"
-                  :min="tempCladeReadsMode === '%' ? 0 : 1"
-                  :max="tempCladeReadsMode === '%' ? 100 : 1000"
-                  variant="underlined"
-                  density="compact"
-                  dense
-                  class="flex-grow-1"
-                  :rules="[valueRangeRule]"
-                  @input="validateRange"
-                  :disabled="showAll"
-                ></v-text-field>
-              </v-container>
-            </v-list-item>
+                    <v-text-field
+                      v-model="tempCladeReadsValue"
+                      type="number"
+                      :min="tempCladeReadsMode === '%' ? 0 : 1"
+                      :max="tempCladeReadsMode === '%' ? 100 : 1000"
+                      variant="underlined"
+                      density="compact"
+                      dense
+                      class="flex-grow-1"
+                      :rules="[valueRangeRule]"
+                      @input="validateRange"
+                      :disabled="showAll"
+                    ></v-text-field>
+                  </v-container>
+                </v-list-item>
 
-            <!-- SLIDER (Taxa Limit) -->
-            <v-list-item>
-              <div class="text-caption">Taxa per level</div>
-              <v-container class="menu-item">
-                <v-slider
-                  class="slider"
-                  v-model="tempTaxaLimit"
-                  thumb-label="always"
-                  :thumb-size="15"
-                  step="1"
-                  :min="1"
-                  :max="maxTaxaLimit"
-                  tick-size="1"
-                  :disabled="showAll"
-                ></v-slider>
-              </v-container>
-            </v-list-item>
+                <!-- SLIDER (Taxa Limit) -->
+                <v-list-item>
+                  <div class="text-caption">Taxa per level</div>
+                  <v-container class="menu-item">
+                    <v-slider
+                      class="slider"
+                      v-model="tempTaxaLimit"
+                      thumb-label="always"
+                      :thumb-size="15"
+                      step="1"
+                      :min="1"
+                      :max="maxTaxaLimit"
+                      tick-size="1"
+                      :disabled="showAll"
+                    ></v-slider>
+                  </v-container>
+                </v-list-item>
+              </v-expansion-panel-text>
+            </v-expansion-panel>
 
-            <v-divider class="py-3"></v-divider>
+            <!-- VISUAL SETTINGS -->
+            <v-expansion-panel title="Visual Settings">
+              <v-expansion-panel-text>
+                <!-- SLIDER (Figure Height) -->
+                <v-list-item>
+                  <div class="text-caption">Figure Height</div>
+                  <v-container>
+                    <v-slider
+                      class="slider"
+                      v-model="tempFigureHeight"
+                      thumb-label="always"
+                      :thumb-size="15"
+                      step="5"
+                      :min="300"
+                      :max="800"
+                    ></v-slider>
+                  </v-container>
+                </v-list-item>
 
-            <!-- SLIDER (Figure Height) -->
-            <v-list-item>
-              <div class="text-caption">Figure Height</div>
-              <v-container>
-                <v-slider
-                  class="slider"
-                  v-model="tempFigureHeight"
-                  thumb-label="always"
-                  :thumb-size="15"
-                  step="5"
-                  :min="300"
-                  :max="800"
-                ></v-slider>
-              </v-container>
-            </v-list-item>
-
-            <!-- LABEL ABOVE NODE (Proportion/Clade Reads) -->
-            <v-list-item>
-              <div class="text-caption">Label Option</div>
-              <v-container>
-                <div class="d-flex align-center flex-column">
-                  <v-btn-toggle
-                    v-model="tempLabelOption"
-                    variant="outlined"
-                    color="indigo"
-                    divided
-                    mandatory
-                  >
-                    <v-btn value="proportion" height="30">Proportion</v-btn>
-                    <v-btn value="cladeReads" height="30">Clade Reads</v-btn>
-                  </v-btn-toggle>
-                </div>
-              </v-container>
-            </v-list-item>
-          </v-list>
-
+                <!-- LABEL ABOVE NODE (Proportion/Clade Reads) -->
+                <v-list-item>
+                  <div class="text-caption">Label Option</div>
+                  <v-container>
+                    <div class="d-flex align-center flex-column">
+                      <v-btn-toggle
+                        v-model="tempLabelOption"
+                        variant="outlined"
+                        color="indigo"
+                        divided
+                        mandatory
+                      >
+                        <v-btn value="proportion" height="30">Proportion</v-btn>
+                        <v-btn value="cladeReads" height="30"
+                          >Clade Reads</v-btn
+                        >
+                      </v-btn-toggle>
+                    </div>
+                  </v-container>
+                </v-list-item>
+              </v-expansion-panel-text>
+            </v-expansion-panel>
+          </v-expansion-panels>
           <v-divider></v-divider>
 
           <!-- APPLY AND CANCEL BUTTONS -->
@@ -197,6 +210,8 @@ export default {
   data() {
     return {
       menu: false,
+      activePanel: [0],
+      isFormValid: true,
 
       showAll: false,
       taxaLimit: this.initialTaxaLimit,
@@ -212,8 +227,6 @@ export default {
       tempShowUnclassified: false,
       tempFigureHeight: 550,
       tempLabelOption: "cladeReads",
-
-      isFormValid: true,
     };
   },
   computed: {
@@ -308,12 +321,21 @@ export default {
 .v-container {
   padding-bottom: 0px;
 }
-.slider {
-  margin-top: 15px;
-}
 
 .select-width {
   width: auto;
   flex: 0 1 auto;
+}
+
+.slider {
+  margin-top: 15px;
+}
+
+/* Expansion Panel */
+.configure-menu .v-list-item {
+  width: 100%;
+}
+::v-deep .configure-menu .v-expansion-panel-text__wrapper {
+  padding: 0px;
 }
 </style>
