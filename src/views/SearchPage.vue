6a80009d839337255d5b9a7e5e23979bf3ab81ef
Sunny (SunJae) Lee 이선재 <snjlee58@gmail.com>
2024-08-15 13:58:00 +0900

    Adjusted style margin for loading dialog

--- src/views/SearchPage.vue
+++ src/views/SearchPage.vue
@@ -13,37 +13,41 @@
       <v-card class="mx-auto" width="700">
         <v-list>
           <!-- Title -->
-          <v-list-item class="font-weight-bold text-h6 pb-0 pl-8 text-button">
+          <v-list-item class="font-weight-bold text-h6 py-0 pl-8 text-button">
             <span>Processing Job...</span>
             <template v-slot:append>
               <v-img src="assets/marv_metabuli_animated.gif" width="60"></v-img>
             </template>
           </v-list-item>
 
-          <!-- Display Real-time Output -->
-          <v-list-item class="pt-1">
-            <template v-slot:subtitle>
-              <v-textarea
-                variant="outlined"
-                v-if="backendOutput"
-                v-model="backendOutput"
-                label="Command Line Output"
-                rows="15"
-                no-resize
-                readonly
-                hide-details="true"
-                bg-color="white"
-                class="mt-3 mx-0 text-caption"
-                ref="outputTextarea"
-              ></v-textarea>
-            </template>
-          </v-list-item>
+          <!-- Hide Log Output + Cancel Button on Load Sample -->
+          <div v-if="!isSampleJob">
+            <!-- Display Real-time Output -->
+            <v-list-item class="pt-1">
+              <template v-slot:subtitle>
+                <v-textarea
+                  variant="outlined"
+                  v-if="backendOutput"
+                  v-model="backendOutput"
+                  label="Command Line Output"
+                  rows="15"
+                  no-resize
+                  readonly
+                  hide-details="true"
+                  bg-color="white"
+                  class="mt-3 mx-0 text-caption"
+                  ref="outputTextarea"
+                ></v-textarea>
+              </template>
+            </v-list-item>
 
-          <!-- Cancel Button -->
-          <div v-if="!isSampleJob" class="d-flex justify-end mr-2">
-            <v-btn variant="plain" color="primary" @click="cancelBackend"
-              >Cancel</v-btn
-            >
+            <!-- Cancel Button -->
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn variant="plain" color="primary" @click="cancelBackend"
+                >Cancel</v-btn
+              >
+            </v-card-actions>
           </div>
         </v-list>
       </v-card>
