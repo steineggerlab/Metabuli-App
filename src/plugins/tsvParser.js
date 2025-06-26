8754d92857e947451266774206d813a7a0c685ff
Sunny (SunJae) Lee 이선재 <snjlee58@gmail.com>
2025-06-26 14:59:49 +0900

    Fixed tsv parse error

--- src/plugins/tsvParser.js
+++ src/plugins/tsvParser.js
@@ -23,8 +23,14 @@ export default {
 		};
 
 		const headers = ["proportion", "clade_reads", "taxon_reads", "rank", "taxon_id", "name"];
-		let records = tsv
-			.split("\n")
+
+		const lines = tsv.split("\n");
+		// If first line is a header row (starts with “#”), remove it
+		if (lines[0].trim().startsWith("#")) {
+			lines.shift();
+		}
+
+		const records = lines
 			.map((line) => {
 				const data = line.split("\t"); // Strip leading and trailing whitespace
 
@@ -37,14 +43,13 @@ export default {
 					record.depth = depth; // Depth based on indentation in front of scientific name in taxonomy report
 					record.nameWithIndentation = record.name; // Store name with indentation from taxonomy report
 					record.name = record.name.trim(); // Remove leading and trailing whitespace after counting leading spaces
-					record.rank = ["superkingdom", "domain"].includes(record.rank) ? "superkingdom" : record.rank; // Replace domain with superkingdom
+					record.rank = record.rank === "superkingdom" ? "domain" : record.rank; // Convert 'superkingdom' to 'domain'
 				}
 
 				return record;
 			})
 			.filter((record) => !Object.values(record).every((field) => field === "" || field === undefined || field === null)); // Filter out empty rows
 
-			records = records.slice(1); // remove the first row before validation
 		// Validate report.tsv file
 		if (validateReportTSVData(records)) {
 			return { results: records };
